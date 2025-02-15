f6042b2751505d2bad00f8fbe3f9eecf4ca64b14
Ramil96 <167094855+Ramil96@users.noreply.github.com>
2025-02-15 18:33:51 +0000

    create genre route

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template, 
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import objectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
 
 @app.route("/")
-def hello():
-    return "Hello World ... again!"
+@app.route("/get_genres")
+def get_genres():
+    genres = mongo.db.genres.find()
+    return render_template("genres.html", genres=genres)
 
 
 if __name__ == "__main__":
