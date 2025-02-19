13e7a64d9a793d3c62b825d1623ed9e772f324f6
Ramil96 <167094855+Ramil96@users.noreply.github.com>
2025-02-19 08:50:05 +0000

    Add register functionality register method

--- app.py
+++ app.py
@@ -27,6 +27,18 @@ def add_books():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+        
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
     return render_template("register.html")
 
 
