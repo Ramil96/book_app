71a5f29f5fdd628edd8c69eff0071a32a5880241
Ramil96 <167094855+Ramil96@users.noreply.github.com>
2025-02-21 17:12:12 +0000

    Add genre dropdown

--- static/js/script.js
+++ static/js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
   $(".sidenav").sidenav({ edge: "right" });
   $(".collapsible").collapsible();
   $(".tooltipped").tooltip();
+  $('select').formSelect();
   $(".datepicker").datepicker({
     format: "dd mmmm, yyyy",
     yearRange: [new Date().getFullYear() - 200, new Date().getFullYear()], // 200 years ago to today
