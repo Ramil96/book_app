242d769757aa84ede25c257a1f886b5e78526feb
Ramil96 <167094855+Ramil96@users.noreply.github.com>
2025-02-21 12:55:13 +0000

    Add task from input fields

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,14 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav({edge: "right"});
-    $('.collapsible').collapsible();
-    $('.tooltipped').tooltip();
+$(document).ready(function () {
+  $(".sidenav").sidenav({ edge: "right" });
+  $(".collapsible").collapsible();
+  $(".tooltipped").tooltip();
+  $(".datepicker").datepicker({
+    format: "dd mmmm, yyyy",
+    yearRange: [new Date().getFullYear() - 200, new Date().getFullYear()], // 200 years ago to today
+    maxDate: new Date(),  // Prevents selecting future dates
+    showClearBtn: true,
+    i18n: {
+      done: "Select"
+    }
   });
+});
